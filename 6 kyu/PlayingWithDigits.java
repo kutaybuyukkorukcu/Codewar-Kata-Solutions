74b0892b3b2d09b3b8a7592e17389aed8ec8d233
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-02-05 22:51:39 +0300

    Create PlayingWithDigits.java

---
+++ 6 kyu/PlayingWithDigits.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class DigPow {
+	
+	public static long digPow(int n, int p) {
+	  ArrayList<Integer> list = new ArrayList<Integer>();
+    int m = n;
+    while (m > 0){
+      list.add(m % 10);
+      m = m / 10;
+    }
+    int toplam = 0,temp = 0;
+    for (Integer item : list){
+      temp = p + list.size() - 1;
+      toplam = toplam + (int) Math.pow(item,temp);
+      --p;
+    }
+    if (toplam % n == 0)
+      return (toplam / n);
+    else
+      return -1;
+  }
+	
+}
