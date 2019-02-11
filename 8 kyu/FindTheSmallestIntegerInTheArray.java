82c3026bdb06a50664fa7a0a93f6aeb0d14ab613
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-02-11 23:47:59 +0300

    Create FindTheSmallestIntegerInTheArray.java

---
+++ 8 kyu/FindTheSmallestIntegerInTheArray.java
@@ -0,0 +1,10 @@
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+      int min = args[0];
+      for(int i = 1; i < args.length; i++){
+      if(min > args[i]) 
+        min = args[i];
+      }
+      return min;
+    }
+}
