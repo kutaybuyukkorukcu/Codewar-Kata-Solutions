21d98acd1b82777bbbb52bad7307066b41f7f952
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-02-26 12:21:36 -0800

    Create WriteNumberInExpandedForm.py

---
+++ 6 kyu/WriteNumberInExpandedForm.py
@@ -0,0 +1,8 @@
+def expanded_form(num):
+    output = []
+    
+    for index, digit in enumerate(str(num)[::-1]):
+        if int(digit) != 0:
+            output.append(digit + ('0' * index))
+
+    return ' + '.join(output[::-1])
