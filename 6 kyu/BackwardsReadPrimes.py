a727e2f5976f275bdb760f4a4bf45b6afe5e7d13
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-02-28 12:57:42 -0800

    Create BackwardsReadPrimes.py

---
+++ 6 kyu/BackwardsReadPrimes.py
@@ -0,0 +1,7 @@
+def prime(n):
+    if n in (2, 3, 5): return True
+    if n % 2 == 0 or n % 3 == 0: return False
+    return all(n % i for i in range(5, int(n**.5)+1, 2))
+        
+def backwardsPrime(start, stop):
+    return [x for x in range(start, stop + 1) if (str(x) != str(x)[::-1]) and prime(x) and prime(int(str(x)[::-1])) ]
