ab01532fdbcd4940d8f64feb896adbfff842494b
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-03-03 12:57:04 -0800

    Create EvilAutocorrectPrank.java

---
+++ 6 kyu/EvilAutocorrectPrank.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+public class Kata {
+  public static String autocorrect(String input) {
+    Pattern you = Pattern.compile("([yY][oO])([uU])+(\\W)*");
+    Pattern u = Pattern.compile("\\s*[uU]\\s*");
+    String[] words = input.split("\\s");
+    for (int i = 0; i < words.length; i++) {
+      Matcher mYou = you.matcher(words[i]);
+      Matcher mU = u.matcher(words[i]);
+      if (mYou.matches()) words[i] = mYou.replaceAll("your sister$3");
+      else if (mU.matches()) words[i] = mU.replaceAll("your sister");
+    }
+    return String.join(" ", words);
+  }
+}
