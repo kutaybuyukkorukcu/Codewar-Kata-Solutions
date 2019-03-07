cbfc298832c98107129217d389acc2f631b47a5a
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-03-07 10:20:30 -0800

    Create Worda10n.java

---
+++ 6 kyu/Worda10n.java
@@ -0,0 +1,16 @@
+import java.util.regex.*;
+public class Abbreviator {
+  public String abbreviate(String s) {
+    Pattern p = Pattern.compile("[a-z]{4,}", Pattern.CASE_INSENSITIVE);
+    Matcher m = p.matcher(s);
+    StringBuffer sb = new StringBuffer();
+    while (m.find()) {
+      String g = m.group();
+      int len = g.length();
+      m.appendReplacement(sb, g.substring(0, 1) + (len - 2) + g.substring(len - 1, len));
+    }
+    m.appendTail(sb);
+    return sb.toString();
+  }
+}
+//chckd
