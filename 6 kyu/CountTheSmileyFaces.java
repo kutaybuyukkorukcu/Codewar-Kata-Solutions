c080297844c566c0f029e16a36a8bfd3dcc73caa
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-03-10 00:00:31 +0300

    Create CountTheSmileyFaces.java

---
+++ 6 kyu/CountTheSmileyFaces.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.*;
+public class SmileFaces {
+  public static int countSmileys(List<String> arr) {
+      int count = 0;
+      String pattern = "[:;][-~]?[)D]";
+      Pattern p = Pattern.compile(pattern);
+      for(String item : arr){
+        Matcher m = p.matcher(item);
+        if(m.matches()) count++;
+      }
+      return count;
+  }
+}
