41befa277f808e11c59ca452b740c5b562e36aad
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-03-12 23:27:03 +0300

    Create TakeATenMinuteWalk.java

---
+++ 6 kyu/TakeATenMinuteWalk.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+public class TenMinWalk {
+  public static boolean isValid(char[] walk) {
+    HashMap<Character,Integer> pair = new HashMap();
+    pair.put('n',1);
+    pair.put('e',1);
+    pair.put('s',-1);
+    pair.put('w',-1);
+    int sum = 0;
+    for(int i  = 0;i<walk.length;i++){
+      char c = walk[i];
+      sum += pair.get(c);
+    }
+    if(sum == 0 && walk.length == 10){
+      return true;
+    }
+    return false;
+  }
+}
