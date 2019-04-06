52ea43cf6c5330cd96d29cbd34e544a2cc8ffefe
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-06 23:44:57 +0300

    Create TripleTrouble.java

---
+++ 6 kyu/TripleTrouble.java
@@ -0,0 +1,17 @@
+public class Kata{
+  public static int TripleDouble(long num1, long num2) {
+		String string1 = String.valueOf(num1);
+		String string2 = String.valueOf(num2);
+
+		for(int i = 0; i < string1.length(); i++){
+			char ch = string1.charAt(i);
+			if(string1.contains(new String(new char[]{ch, ch, ch})) &&
+					string2.contains(new String(new char[]{ch, ch}))){
+				return 1;
+			}
+		}
+		return 0;
+  }
+}
+
+//chkd
