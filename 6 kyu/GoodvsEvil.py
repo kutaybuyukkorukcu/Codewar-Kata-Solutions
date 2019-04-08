b23a6df8a0439cb49cac822f9ac47f054180ca13
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-08 23:12:39 +0300

    Create GoodvsEvil.py

---
+++ 6 kyu/GoodvsEvil.py
@@ -0,0 +1,13 @@
+def goodVsEvil(good, evil):
+    gList = [1, 2, 3, 3, 4, 10]
+    eList = [1, 2, 2, 2, 3, 5, 10]
+    goodList = good.split(" ")
+    evilList = evil.split(" ")
+    g = sum(gList[en]*i for en, i in enumerate(map(int, goodList)))
+    e = sum(eList[en]*i for en, i in enumerate(map(int, evilList)))
+    if g > e:
+        return "Battle Result: Good triumphs over Evil"
+    if g < e:
+        return "Battle Result: Evil eradicates all trace of Good"
+    else:
+        return "Battle Result: No victor on this battle field"
