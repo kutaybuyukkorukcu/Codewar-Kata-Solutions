96b326c34ba843983146e623ac883189c8ac36d0
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-10 23:48:00 +0300

    Create TortoiseRacing.java

---
+++ 6 kyu/TortoiseRacing.java
@@ -0,0 +1,11 @@
+public class Tortoise {
+    public static int[] race(int v1, int v2, int g) {
+      float answer = (float)g/(v2-v1);
+      if(answer < 0) return null;
+      int[] solution = new int[3];
+      solution[0] = (int)answer;
+      solution[1] = (int)((answer-solution[0])*60);
+      solution[2] = (int)((answer-solution[0]-solution[1]/60.0)*3600.0);
+      return solution;
+    }
+}
