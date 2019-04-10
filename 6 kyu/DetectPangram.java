c74d847c02c70a15731dfbefab6b567a4d218555
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-10 23:47:46 +0300

    Create DetectPangram.java

---
+++ 6 kyu/DetectPangram.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+public class PangramChecker {
+  public boolean check(String sentence){
+    String newS = sentence.replaceAll("[^A-Za-z]", "");
+    Set<String> letters = new HashSet<>(Arrays.asList(newS.split("")));
+    return letters.size() >= 26;
+  }
+}
