3b661a6ae049ecb564ff0e2f666b84d9f8907e2f
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-13 23:58:35 +0300

    Create BuildTower.py

---
+++ 6 kyu/BuildTower.py
@@ -0,0 +1,9 @@
+def tower_builder(n_floors):
+    floors = []
+    n = n_floors
+    for i in range(n_floors):
+        n -= 1
+        floors.append(' ' * n + '*' * (i * 2 + 1) + ' ' * n)
+    return floors
+    
+#chkd
