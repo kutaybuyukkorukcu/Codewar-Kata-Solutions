cb0e67b96afe28d22bdbea49c7a892aea53a26db
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-14 23:47:10 +0300

    Create CountingDuplicates.java

---
+++ 6 kyu/CountingDuplicates.java
@@ -0,0 +1,15 @@
+public class CountingDuplicates {
+  public static int duplicateCount(String text) {
+        int ans = 0;
+    text = text.toLowerCase();
+    while (text.length() > 0) {
+      String firstLetter = text.substring(0,1);
+      text = text.substring(1);
+      if (text.contains(firstLetter)) ans ++;
+      text = text.replace(firstLetter, "");
+    }
+    return ans;
+  }
+}
+
+//chkd
