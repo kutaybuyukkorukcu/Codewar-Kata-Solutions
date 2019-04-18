f1def3f1fb85717181b20d2612a3b36f7c5e934f
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-18 21:13:13 +0300

    Create FormatAStringOfNamesLike.py

---
+++ 6 kyu/FormatAStringOfNamesLike.py
@@ -0,0 +1,9 @@
+def namelist(names):
+    if len(names) > 1:
+        return '{} & {}'.format(', '.join(name['name'] for name in names[:-1]), names[-1]['name'])
+    elif names:
+        return names[0]['name']
+    else:
+        return ''
+        
+#chkd
