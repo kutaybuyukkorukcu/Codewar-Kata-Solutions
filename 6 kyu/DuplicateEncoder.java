3187d2a9f740f0c6619e9780fb7f424bff453c52
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-19 23:31:42 +0300

    Create DuplicateEncoder.java

---
+++ 6 kyu/DuplicateEncoder.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+import java.util.HashMap;
+public class DuplicateEncoder {
+  public static String encode(String phrase) {
+        Map<Character, Integer> map = new HashMap<>();
+    phrase = phrase.toLowerCase();
+
+    for(int i = 0; i < phrase.length(); i++){
+      if(map.containsKey(phrase.charAt(i))){
+        int value = map.get(phrase.charAt(i));
+        map.remove(phrase.charAt(i));
+        map.put(phrase.charAt(i), ++value);
+
+      } else {
+        map.put(phrase.charAt(i), 1);
+      }
+    }
+
+    String output = "";
+
+    for(int i = 0; i < phrase.length(); i++){
+      output += (map.get(phrase.charAt(i)) > 1) ? ")" : "(";
+    }
+
+    return output;
+}}
+
+//chkd
