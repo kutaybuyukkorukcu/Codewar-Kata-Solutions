a16b82f4e61febad4e54f00b5b156af5973dd061
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-20 20:31:53 +0300

    Create WhoLikesIt.java

---
+++ 6 kyu/WhoLikesIt.java
@@ -0,0 +1,20 @@
+class Solution {
+  public static String whoLikesIt(String... names) {
+	  int count = names.length;
+	  String r = "";	
+	  if(count>=4){
+		  r = names[0] + ", " + names[1] + " and " + (count - 2) + " others like this"; 
+    }else if(count == 1){
+		  r = names[0] + " likes this";
+    }else if(count == 2){
+      r = names[0] + " and " + names[1] + " like this";
+    }else if(count == 3){
+      r = names[0] + ", " + names[1] + " and " + names[2] + " like this";
+    }else if(count == 0){
+      r = "no one likes this";
+    }
+	return r;
+  }
+}
+
+//chkd
