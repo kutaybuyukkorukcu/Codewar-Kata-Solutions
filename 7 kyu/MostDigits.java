9dad8cb039a74ca4f10279aa4e0b6c1fba6187e8
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-04-24 23:48:05 +0300

    Create MostDigits.java

---
+++ 7 kyu/MostDigits.java
@@ -0,0 +1,9 @@
+public class MostDigits {
+  public static int findLongest(int[] numbers) {
+      int index = 0;
+    for (int i = 0; i < numbers.length; i++) 
+      if (String.valueOf(Math.abs(numbers[i])).length() > String.valueOf(Math.abs(numbers[index])).length()) 
+        index = i;
+    return numbers[index];
+  }
+}
