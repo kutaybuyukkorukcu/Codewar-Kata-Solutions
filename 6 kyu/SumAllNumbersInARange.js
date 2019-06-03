ffa73e1a79060163e7a544f36886dce12ff66fa1
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-06-03 23:44:02 +0300

    Create SumAllNumbersInARange.js

---
+++ 6 kyu/SumAllNumbersInARange.js
@@ -0,0 +1,9 @@
+/* Return the sum of those two numbers plus the sum of all the numbers between them. */
+
+function sumAll(arr) {
+    var sum = 0;
+    for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
+        sum += i;
+    }
+  return sum;
+}
