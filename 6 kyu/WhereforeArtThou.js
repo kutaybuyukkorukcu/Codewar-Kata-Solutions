e83474098d78cbd43c49b59193a8e146acabb517
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-06-04 00:01:00 +0300

    Create WhereforeArtThou.js

---
+++ 6 kyu/WhereforeArtThou.js
@@ -0,0 +1,24 @@
+/**/
+
+function whatIsInAName(collection, source) {
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
+  var srcKeys = Object.keys(source);
+
+  return collection.filter(function (obj) {
+    return srcKeys.every(function (key) {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+
+// Alternative
+
+(return srcKeys
+      .map(function(key) {
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      })
+      .reduce(function(a, b) {
+        return a && b;
+      })
