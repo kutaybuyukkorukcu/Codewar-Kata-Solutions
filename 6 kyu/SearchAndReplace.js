0f57662fe917e12c1d26e375611305778b488f4c
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-06-08 23:35:42 +0300

    Create SearchAndReplace.js

---
+++ 6 kyu/SearchAndReplace.js
@@ -0,0 +1,18 @@
+function myReplace(str, before, after) {
+
+  function applyCasing(source, target) {
+    var targetArr = target.split("");
+    var sourceArr = source.split("");
+    for (var i = 0; i < Math.min(targetArr.length, sourceArr.length); i++){
+      if (/[A-Z]/.test(sourceArr[i])) {
+        targetArr[i] = targetArr[i].toUpperCase();
+      }else{ 
+        targetArr[i] = targetArr[i].toLowerCase();
+      }
+    } 
+
+    return (targetArr.join(""));
+  }
+
+  return str.replace(before, applyCasing(before, after));
+}
