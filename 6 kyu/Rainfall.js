b36cea926e2f4c290f2aa47c44a2b10c6849b30d
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-06 23:39:01 +0300

    Update Rainfall.js

--- 6 kyu/Rainfall.js
+++ 6 kyu/Rainfall.js
@@ -1,10 +1,30 @@
+function calculateData(inpt, strng, calulateCallback) {
+    if (!inpts.some(t => t == inpt)) {
+        return -1;
+    }
+
+    const allData = strng.split('\n');
+    for (const data of allData) {
+        if (data.indexOf(inpt) !== 0) {
+            continue;
+        }
+
+        const monthlyData = data.replace(/[a-zA-Z\:\s]+/g, '')
+            .split(',')
+            .map(Number.parseFloat);
+        return calulateCallback(monthlyData);
+    }
+
+    return -1;
+}
+
 function mean(town, strng) {
-    return calculateData(town, strng, d => d.reduce((p, c) => p + c) / d.length);
+    return calculateData(inpt, strng, d => d.reduce((p, c) => p + c) / d.length);
 }
 
 function variance(town, strng) {
     const avg = mean(town, strng);
-    return calculateData(town, strng, d => d.reduce((p, c) => p + Math.pow(c - avg, 2) , 0) / d.length);
+    return calculateData(inpt, strng, d => d.reduce((p, c) => p + Math.pow(c - avg, 2) , 0) / d.length);
 }
 
 #Will cont
