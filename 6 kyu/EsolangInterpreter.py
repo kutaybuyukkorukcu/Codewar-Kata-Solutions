ab04c667aad13ebafde081987de0cb8ab6cba7b6
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-06 23:43:06 +0300

    Update EsolangInterpreter.py

--- 6 kyu/EsolangInterpreter.py
+++ 6 kyu/EsolangInterpreter.py
@@ -1,7 +1,8 @@
-def my_first_interpreter(input)
-  output = []
-  cell = 0
-  commands = input.delete('^+.')
-  increment = commands.scan(/(^\.+)/)
-
-#Will cont
+def my_first_interpreter(code):
+    input, output = 0, " "
+    for v in code:
+        if v in '+':
+          input = (input + 1) % 256
+        elif v in '.': 
+          output += chr(input)
+    return output
