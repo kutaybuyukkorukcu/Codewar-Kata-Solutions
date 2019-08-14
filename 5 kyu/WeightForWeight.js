00e28cee152ea2a967c7fcab516f9a74b3728d40
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-14 23:57:52 +0300

    Create WeightForWeight.js

---
+++ 5 kyu/WeightForWeight.js
@@ -0,0 +1,14 @@
+function orderWeight(str) {
+  return str
+    .split(' ')
+    .map(item => {
+      const weight = item.split('').reduce((a, b) => Number(a) + Number(b), 0)
+      return { item, weight };
+    })
+    .sort(
+      (a, b) =>
+        a.weight === b.weight ? a.item.localeCompare(b.item) : a.weight - b.weight
+    )
+    .map(({ item }) => item)
+    .join(' ');
+}
