9757d3a2b6e0ae0d8b3af4eda1eaae6aae26dcea
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-15 23:59:56 +0300

    Create IssuesWithMath.js

---
+++ 5 kyu/IssuesWithMath.js
@@ -0,0 +1,11 @@
+Math.round = number => {
+  return (number - parseInt(number) >= 0.5) ? parseInt(number) + 1 : parseInt(number) ;
+};
+
+Math.ceil = number => {
+  return (parseInt(number) === number) ? number : parseInt(number) + 1;
+};
+
+Math.floor = number => {
+  return parseInt(number);
+};
