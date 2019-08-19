c9a17d51efda69124c85f224c8d91de529308816
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-19 23:51:55 +0300

    Create NonRepeatingCharacter.js

---
+++ 5 kyu/NonRepeatingCharacter.js
@@ -0,0 +1,17 @@
+function firstNonRepeatingLetter(s) {
+
+  const reducer = (accur, current) => {
+    let i = current.toLowerCase();
+    console.log(i + "!");
+    accur[i] ? accur[i][1] += 1 : accur[i] = [current, 1];
+    return accur;
+  }
+  
+  const obj = s
+    .split("")
+    .reduce(reducer, {});
+  
+  const result = Object.values(obj).find(el => el[1] === 1);
+    
+  return result ? result[0] : '';
+}
