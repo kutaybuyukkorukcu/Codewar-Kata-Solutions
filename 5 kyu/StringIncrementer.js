6634ac26f6978a9fb307041892dcf682c8e43c4b
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-23 23:16:47 +0300

    Create StringIncrementer.js

---
+++ 5 kyu/StringIncrementer.js
@@ -0,0 +1,10 @@
+function incrementString(input) {
+  if(isNaN(parseInt(input[input.length - 1]))) {
+    return input + "1";
+  }
+  
+  return input.replace(/(0*)([0-9]+$)/, function(match, p1, p2) {
+    const num = parseInt(p2) + 1;
+    return num.toString().length > p2.length ? p1.slice(0, -1) + num : p1 + num;
+  });
+}
