b3c5409594a4ddec107818feacaeda09dc523972
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-25 23:50:53 +0300

    Create MemoizedFibo.js

---
+++ 5 kyu/MemoizedFibo.js
@@ -0,0 +1,17 @@
+function wrap(func) {
+  const cache = {};
+  
+  return (n) => {
+    if (!cache[n]) {
+      cache[n] = func(n);
+    }
+    return cache[n];
+  }
+}
+
+const fibonacci = wrap((n) => {
+  if (n == 0 || n == 1) {
+    return n;
+  }
+  return fibonacci(n-1) + fibonacci(n-2);
+})
