51a16873be21460b01402d66ae5ad476f2969a8a
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-08-28 23:24:59 +0300

    Update SumOfPairs.js

--- 5 kyu/SumOfPairs.js
+++ 5 kyu/SumOfPairs.js
@@ -1,18 +1,17 @@
 var sum_pairs = function(ints, s) {
-    var map = {}, pairMaxIndex = ints.length - 1, pair;
-
-    for (let i = 0; i <= pairMaxIndex; i++) {
-        var a = ints[i];
-        var b = s - a;
-        var j = map[b];
-        if (j !== undefined && i <= pairMaxIndex && j <= pairMaxIndex) {
-            pairMaxIndex = i > j ? i : j;
-            pair = i < j ? [a, b] : [b, a];
-        }
-        var tmp = map[a];
-        if (tmp === undefined || i < tmp) {
-            map[a] = i;
-        }
+    let arr = [], result = [];
+    
+    ints.forEach(n => {
+      if (arr.includes(s - n)  && !result.length) {
+        result = [s-n, n];     
+      }
+      arr.push(n);
+    });
+    
+    if (!result.length) {
+      return undefined;
     }
-    return pair;
+    return result;
 };
+
+//:vault-boy-thumbs-up:
