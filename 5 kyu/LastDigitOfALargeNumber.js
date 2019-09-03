e0fb9bc5e164abac965e8d3f173fb01ecf17c604
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2019-09-03 23:59:45 +0300

    Create LastDigitOfALargeNumber.js

---
+++ 5 kyu/LastDigitOfALargeNumber.js
@@ -0,0 +1,7 @@
+var lastDigit = function(str1, str2){
+  let fNum = +str1.slice(-1);
+  let sNum = +str2.slice(-2);
+  return +str2 === 0 ? 1 : Math.pow(fNum, sNum % 4 + 4) % 10
+}
+
+// + converts strings to nums
