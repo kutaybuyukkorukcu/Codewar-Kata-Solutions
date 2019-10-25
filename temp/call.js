25005bd93e00ca9fb21190ef8c23b25c128cb56c
kutaybuyukkorukcu <kutaybuyukkorukcu@gmail.com>
2019-10-25 14:35:02 +0300

    deleted some unneces files

--- temp/call.js
+++
@@ -1,4 +0,0 @@
-// Given a key and a set of arguments, call them when given a context. Primarily useful in composition.
-// Use a closure to call a stored key with stored arguments.
-
-const call = (key, ...args) => context => context[key](...args);
--- temp/overArgs.js
+++
@@ -1,4 +0,0 @@
-// Creates a function that invokes the provided function with it's arguments transformed.
-
-const overArgs = (f, transforms) => (...args) => 
-                                f(...args.map((value, i) => transforms[i](value)));
